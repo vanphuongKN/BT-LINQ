497765b90b3a0175b3c73b3db9e80b1acb161224
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 02:06:24 +0700

    Bài 4. Sắp xếp giảm dần

---
+++ NopBai/Bai4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+            List<int> dayso = new List<int> { 1, 5, 2, 8, 3, 7, 4, 6 };
+            var sapXepGiam = dayso.OrderByDescending(n => n);
+            Console.WriteLine("Danh sach giam dan: " + string.Join(", ", sapXepGiam));
+
+
+        }
+    }
+}
