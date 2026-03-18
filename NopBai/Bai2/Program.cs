d4b903a08c4093b3309de36d3454dad4e5c7ce33
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 01:31:40 +0700

    Bài 2. Lọc số lớn hơn 5

---
+++ NopBai/Bai2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+
+            List<int> dayso = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var lonHon5 = dayso.Where(n => n > 5).ToList();
+
+            Console.WriteLine(string.Join(", ", lonHon5));
+        }
+    }
+}
