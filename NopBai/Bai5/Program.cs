758f6c12888078075ff6809772fc7165cc3c4c10
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 02:18:16 +0700

    Bài 5. Bình phương các số

---
+++ NopBai/Bai5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 1, 2, 3, 4, 5 };
+            var binhPhuong = dayso.Select(n => n * n);
+            Console.WriteLine("Danh sach ban dau: " + string.Join(" ", dayso));
+            Console.WriteLine("Danh sach binh phuong: " + string.Join(" ", binhPhuong));
+
+        }
+    }
+}
