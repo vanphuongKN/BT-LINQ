f21c2ef9dad1f226732608ca56ff28fe6ce812a6
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 02:04:12 +0700

    Bài 3. Sắp xếp tăng dần

---
+++ NopBai/Bai3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 8, 3, 1, 6, 2, 7, 4, 5 };
+            var sapXepTang = dayso.OrderBy(n => n);
+            Console.WriteLine("Danh sách tang dan: " + string.Join(", ", sapXepTang));
+
+        }
+    }
+}
