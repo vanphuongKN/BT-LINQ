0a675048c76c4715bc2ae5340ea9c49736267fb4
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 02:29:50 +0700

    Bài 7. Kiểm tra có số lớn hơn 10 không

---
+++ NopBai/Bai7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Bai7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 2, 4, 6, 8, 11, 3 };
+            bool coSoLonHon10 = dayso.Any(n => n > 10);
+            Console.WriteLine("Danh sach: " + string.Join(", ", dayso));
+
+            Console.WriteLine("Co so nao lon hon 10 khong: " + coSoLonHon10);
+
+
+        }
+    }
+}
