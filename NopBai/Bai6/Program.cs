cb54fc11e0f589e26b91527b7450b2a8dd5d9250
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 02:22:50 +0700

    Bài 6. Đếm số chẵn

---
+++ NopBai/Bai6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int soLuongChan = dayso.Count(n => n % 2 == 0);
+            Console.WriteLine("Danh sach: " + string.Join(", ", dayso));
+            Console.WriteLine("So luong phan tu chan la: " + soLuongChan);
+        }
+    }
+}
