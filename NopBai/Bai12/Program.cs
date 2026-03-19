d9f11a32eb8ab45deccca278bd9d283f933c323b
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 14:01:05 +0700

    Bài 12. Đếm số sinh viên đạt

---
+++ NopBai/Bai12/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Bai12
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC#02");
+
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1,Name = "An", Score = 8 },
+                new Student { Id = 2,Name = "Binh ", Score = 6 },
+                new Student { Id = 3,Name = "Chi ", Score = 9 },
+                new Student { Id = 4,Name = "Dung", Score = 7 }
+            };
+            int soluongDat = students.Count(s => s.Score >= 7);
+            Console.WriteLine("So luong sinh vien co diem lon hon 7 la: " + soluongDat);
+
+        }
+    }
+}
