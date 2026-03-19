7b607950372e04f95776413e8d4a97a70cc52548
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 18:27:59 +0700

    Bài 17. Tính trung bình

---
+++ Bai17/Bai17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Bai17
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<Student> students = new List<Student>() {
+                new Student{Id=1, Name="An", Score=8},
+                new Student{Id=2, Name="Binh", Score=6},
+                new Student{Id=3, Name="Chi", Score=9},
+                new Student{Id=4, Name="Dung", Score=7}
+            };
+            double diemTrungBinh = students.Average(s => s.Score);
+            Console.WriteLine("Diem trung binh cua ca lop la: " + diemTrungBinh);
+        }
+    }
+}
