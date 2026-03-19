e9719fb69f37c77158d6f2c2e4ceba20e5f624fc
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 18:29:57 +0700

    Bài 19. Tìm số xuất hiện nhiều nhất

---
+++ Bai19/Bai19/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Bai19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 1, 5, 2, 3, 3, 4, 3, 5, 2, 3, 1 };
+            var soXuatHienNhieuNhat = dayso.GroupBy(n => n).OrderByDescending(g => g.Count()) .FirstOrDefault();
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", dayso));
+            if (soXuatHienNhieuNhat != null)
+            {
+                Console.WriteLine($"So xuat hien nhieu nhat la: {soXuatHienNhieuNhat.Key}");
+                Console.WriteLine($"So lan xuat hien: {soXuatHienNhieuNhat.Count()} lan");
+            }
+            else
+            {
+                Console.WriteLine("Danh sach rong!");
+            }
+        }
+    }
+}
