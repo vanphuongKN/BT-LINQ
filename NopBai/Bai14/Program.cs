65d662f49997576ef238a0c503dba6446e4a383f
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 14:02:26 +0700

    Bài 14. Kiểm tra có sinh viên rớt không

---
+++ NopBai/Bai14/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Bau14
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC#02");
+
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1,Name = "An", Score = 8 },
+                new Student { Id = 2,Name = "Binh ", Score = 6 },
+                new Student { Id = 3,Name = "Chi ", Score = 9 },
+                new Student { Id = 4,Name = "Dung", Score = 7 }
+            };
+            bool coSvRot = students.Any(s => s.Score < 5);
+            if (coSvRot)
+            {
+                Console.WriteLine("Co sinh vien bi rot (diem < 5).");
+            }
+            else
+            {
+                Console.WriteLine("Khong co sinh vien nao rot.");
+            }
+        }
+    }
+}
