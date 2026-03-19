35b7b09054373a7ca79302b4add08c607b2c8de6
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 18:27:09 +0700

    Bài 16. Tính tổng

---
+++ Bai16/Bai16/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+            List<int> dayso = new List<int> { 1, 2, 3, 4, 5 };
+            int tong = dayso.Sum();
+            Console.WriteLine("Danh sach: " + string.Join(" + ", dayso));
+            Console.WriteLine("Tong cac phan tu la: " + tong);
+        }
+    }
+}
