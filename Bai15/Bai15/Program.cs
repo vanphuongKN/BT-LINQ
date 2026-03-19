7a46caeceb8db33fdbd126c6ff48fccd1b205476
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 18:25:58 +0700

    Bài 15. Loại bỏ phần tử trùng

---
+++ Bai15/Bai15/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSV: 2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC02#");
+
+            List<int> dayso = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            var danhSachDuyNhat = dayso.Distinct().ToList();
+            Console.WriteLine("Danh sach ban dau: " + string.Join(" ", dayso));
+            Console.WriteLine("Danh sach duy nhat: " + string.Join(" ", danhSachDuyNhat));
+
+        }
+    }
+}
