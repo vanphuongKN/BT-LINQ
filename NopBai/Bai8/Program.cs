706f0ba2051c5d834379f0973d3b596c7b09d550
Van Phuong <email_cua_ban@gmail.com>
2026-03-19 13:55:04 +0700

    Bài 8. Lọc sinh viên điểm >= 8

---
+++ NopBai/Bai8/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Bai8
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2415053122230");
+            Console.WriteLine("Vo Van Phuong - 225LTC#02");
+
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1,Name = "An", Score = 8 },
+                new Student { Id = 2,Name = "Binh ", Score = 6 },
+                new Student { Id = 3,Name = "Chi ", Score = 9 },
+                new Student { Id = 4,Name = "Dung", Score = 7 }
+            };
+
+            var svGioi = students.Where(s => s.Score >= 8);
+            foreach (var sv in svGioi)
+            {
+                Console.WriteLine($"ID: {sv.Id}, Ten: {sv.Name}, Diem: {sv.Score} ");
+               
+            }
+        }
+    }
+}
